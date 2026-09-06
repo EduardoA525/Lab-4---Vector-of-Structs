996ef07ec5690db1b1b9f9f55f5a864c7dd4ff28
Mystery525 <avilaed525@gmail.com>
2026-09-05 17:35:24 -0700

    Milestone 1 complete

--- Lab4.cpp
+++ Lab4.cpp
@@ -2,3 +2,32 @@
 //COMSC - 210 - 5293
 //Lab 4 - Vector of Structs
 
+//Milestone 1: the struct works and can received initialization data. 
+//Output the object's data to the console for confirmation.
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+//Struct with three color values
+struct Color{
+    int red;
+    int green;
+    int blue;
+};
+
+int main(){
+
+    //Object for testing to console
+    Color testColor = {192, 182, 96};
+
+    //Outputs each value of object testColor
+    cout << "Red Value: " << testColor.red << endl;
+    cout << "Green Value: " << testColor.green << endl;
+    cout << "Blue Value: " << testColor.blue << endl;
+
+    cout << "TEST COMPLETE! :)" << endl;
+
+    return 0;
+}
